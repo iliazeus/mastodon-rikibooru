e90ea379a13c3564059253f6b2bae55325a59005
Ilia Pozdnyakov <iliazeus@proton.me>
2024-04-22 19:23:01 +0500

    add POST_IMMEDIATELY optional env var

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import { env } from "node:process";
+
 import * as bot from "./bot.js";
+
+const { POST_IMMEDIATELY = false } = env;
 
 main();
 
 async function main() {
   await bot.init();
+  if (POST_IMMEDIATELY) await bot.tick();
   setInterval(bot.tick, 2 * 60 * 60 * 1000);
 }
